60dd2eb8cadbe29273a392032344e1823bcf5548
govind <govindsuryavanshi013@gmail.com>
2023-05-13 15:12:12 +0530

    Architect Asian development

--- src/blocks_program/Test_block.java
+++ src/blocks_program/Test_block.java
@@ -7,9 +7,20 @@ public class Test_block {
 	Test_block()
 	{
 		System.out.println("0-arg constructor");
+		System.out.println("0-arg constructor");
+		System.out.println("0-arg constructor");
 	}
 	
 	Test_block(int a)
+	{
+		System.out.println("1-arg constructor");
+		System.out.println("1-arg constructor1");
+		System.out.println("1-arg constructor2");
+		
+		
+	}
+	
+	Test_block(int a,int b)
 	{
 		System.out.println("1-arg constructor");
 		System.out.println("1-arg constructor1");
@@ -33,6 +44,8 @@ public class Test_block {
 		
            new 	Test_block();
            new 	Test_block(10);
+           new 	Test_block(10,20);
+           
            }
 
 }
